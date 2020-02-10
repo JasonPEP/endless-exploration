e62801a427d029e4e11207c6e78fc0004cfa39ba
JasonPEP <jasonsuler@gmail.com>
2020-02-10 16:59:05 +0800

    Optimize straight insertion sort add sentry to protect array border.
    Signed-off-by: JasonPEP <jasonsuler@gmail.com>

--- java/org/jasonpep/sort/StraightInsertionSort.java
+++ java/org/jasonpep/sort/StraightInsertionSort.java
@@ -12,6 +12,7 @@ public class StraightInsertionSort {
     public static void main(String[] args) {
         StraightInsertionSort straightInsertionSort = new StraightInsertionSort();
         straightInsertionSort.sort(new int[]{8, 31, 4, 3, 1, 66, 3, 9, 34, 62});
+        straightInsertionSort.sort1(new int[]{8, 31, 4, 3, 1, 66, 3, 9, 34, 62});
     }
 
     public int[] sort(int[] array) {
@@ -22,6 +23,30 @@ public class StraightInsertionSort {
                 array[j] = array[j - 1];
             }
             array[j] = temp;
+        }
+        return array;
+    }
+
+    public int[] sort1(int[] array) {
+        int minIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[minIndex] > array[i]) {
+                minIndex = i;
+            }
+        }
+        int temp = array[minIndex];
+        array[minIndex] = array[0];
+        array[0] = temp;
+        // ----------把最小值移到无序表的最左边作为哨兵--------------------
+
+
+        for (int i = 2; i < array.length; i++) {
+            int j = i;
+            int temp2 = array[j];
+            for (; temp2 < array[j - 1]; j--) {// 这里因为哨兵的作用 少了一个 数组的边界判断
+                array[j] = array[j - 1];
+            }
+            array[j] = temp2;
         }
         return array;
     }
